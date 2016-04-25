eaf54a76eae6eb1384b4f90d691fbe0890dd5a93
paulhhallam <paul.hallam@gmail.com>
2016-04-25 20:02:23 +0100

    Update sainsb.go

--- cookbooks/goapp/files/default/sainsb.go
+++ cookbooks/goapp/files/default/sainsb.go
@@ -6,7 +6,7 @@ import (
 )
 func handler(w http.ResponseWriter, r *http.Request) {
  h, _:= os.Hostname()
- fmt.Fprintf(w, "Hi there, I'm served from HOST %s!", h)
+ fmt.Fprintf(w, "Hi there, I'm served from HOSTNAME %s!", h)
 }
 func main() {
  http.HandleFunc("/",handler)
